5a0197295c86b48821d4496d32063b2da301a791
Dana Bennett <danabennett10@gmail.com>
2019-03-09 13:48:40 -0500

    Add path data

--- src/App.js
+++ src/App.js
@@ -3,18 +3,63 @@ import vendors from './vendors.json';
 // import './App.css';
 import PanZoom from '@ajainarayanan/react-pan-zoom';
 
+function filterVendors(filter) {
+  console.log(vendors, filter);
+  return vendors.filter(vendor => {
+    return filter[0] === vendor.value;
+  });
+}
 class App extends Component {
-  state = { zoom: 1 };
+  state = {
+    vendors: filterVendors(['🚺']),
+    recording: false,
+    points: [],
+    zoom: 1,
+    filter: [],
+  };
+  points = {};
   handleZoomIn = () => {
     this.setState({ zoom: this.state.zoom + 0.3 });
   };
   handleZoomOut = () => {
     this.setState({ zoom: this.state.zoom - 0.3 });
   };
+
+  handleClick = e => {
+    if (!this.state.recording) {
+      console.log('recording');
+      this.setState({ recording: true, startTime: +new Date() });
+    } else {
+      this.setState({ recording: false });
+      console.log(JSON.stringify(this.points));
+      this.points = {};
+    }
+  };
+
+  handleMouseMove = e => {
+    var rect = e.target.getBoundingClientRect();
+    var x = Math.floor((e.clientX - rect.left) / this.state.zoom); //x position within the element.
+    var y = Math.floor((e.clientY - rect.top) / this.state.zoom); //y position within the element.
+
+    if (this.state.recording) {
+      this.points[+new Date() - this.state.startTime] = {
+        x,
+        y,
+      };
+    }
+  };
+
   render() {
-    const { zoom } = this.state;
+    const { zoom, recording } = this.state;
+    console.log(this.state.vendors);
+
     return (
-      <div className="App">
+      <div
+        className="App"
+        tabIndex="0"
+        onDoubleClick={this.handleClick}
+        onMouseMove={this.handleMouseMove}
+      >
         <PanZoom zoom={zoom}>
           <header className="App-header">
             <img
@@ -30,7 +75,7 @@ class App extends Component {
                 left: '0px',
               }}
             />
-            {vendors.map(vendor => (
+            {this.state.vendors.map(vendor => (
               <div style={{ position: 'absolute', ...vendor.style }}>
                 {vendor.value}
               </div>
